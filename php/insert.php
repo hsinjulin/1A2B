7c7b09c3d277e4eba89f9a4ae659850ec9993270
linhsinju <11010148@gm.chihlee.edu.tw>
2023-09-07 16:13:45 +0800

    Modify to streamlined version in insert.php

--- php/insert.php
+++ php/insert.php
@@ -1,18 +1,25 @@
 <?php
     session_start();
+
     $id = $_POST['player'];
     $guess = $_POST['guess'];
+
     $_SESSION['guess'] = $guess;
+
     $randomNumbers = $_SESSION['randomNumbers'];
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $_SESSION["player"] = $_POST["player"];
-        $_SESSION["guess"] = $_POST["guess"];
+        $_SESSION["player"] = $id;
+        $_SESSION["guess"] = $guess;
     }
     
-    $guessString = str_split($guess);
+    
     $A = 0;
-    $ans_appear_times[10] = array();
-    $guess_appear_times[10] = array();
+    $ans_appear_times = array_fill(0, 10, 0);
+    $guess_appear_times = array_fill(0, 10, 0);
+
+    $guessString = str_split($guess);
+
     //計算有幾個A
     for($i=0; $i< 4; $i++) {
 		
@@ -27,20 +34,12 @@
     //計算有幾個B(已排除計算過的A)
     $B = 0;
     for($i=0; $i<10; $i++) { 
-        if($guess_appear_times[$i] >= $ans_appear_times[$i]) {
-            $B += $ans_appear_times[$i];
-        }
-        else {
-            $B += $guess_appear_times[$i];
-        }
+        $B +=  min($guess_appear_times[$i], $ans_appear_times[$i]);
     }
     
-    //$results = array();
     $results = isset($_SESSION['results']) ? $_SESSION['results'] : array();
-    $result = $guess . ">>" . $A ."A" . $B . "B";  
-    $results[] = $result;
+    $results[] = "$guess >> {$A}A{$B}B";  
     $_SESSION['results'] = $results;
-    //echo "<script>window.location.href = 'php/result.php?result=" . urlencode($result) . "';</script>";
 
     header("Location:index.php");
     
