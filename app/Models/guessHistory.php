9739d7b2a5cdc8fa72d9f8b434306b2eea56e909
LINHSINJU <11010148@gm.chihlee.edu.tw>
2023-09-19 22:42:03 +0800

    設定timestamps

--- app/Models/guessHistory.php
+++ app/Models/guessHistory.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class guessHistory extends Model
 {
     use HasFactory;
+    public $timestamps = false;
     protected $table = 'guesses';
     protected $fillable =['id','guess','result'];
 }
