dbeae23685bb9295e121c8a7ab005fa2eeadbe45
LINHSINJU <11010148@gm.chihlee.edu.tw>
2023-09-19 22:38:32 +0800

    設定路徑

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,12 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
-
 Route::get('/', function () {
     return view('welcome');
-});
+});*/
+Route::get('/', [BooksController::class, 'img'])->name('start');
+Route::get('/books/img', [BooksController::class, 'img'])->name('img');
+Route::get('/books/guess', [BooksController::class, 'guess'])->name('guess');
+Route::get('/books/insert', [BooksController::class, 'insert'])->name('insert');
+Route::get('/books/show', [BooksController::class, 'show'])->name('show');
+Route::get('/books/clear', [BooksController::class, 'clear'])->name('clear');
